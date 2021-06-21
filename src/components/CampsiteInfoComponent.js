baac3cf87266f7d215b19add4fb79fdb0fddff6c
StevenVallarsa <orionsjv@gmail.com>
2021-06-20 21:47:57 -0400

    Week 3 - presentational and container components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -43,21 +43,14 @@ export default class CampsiteInfo extends Component {
   render() {
     if (this.props.campsite) {
       return (
-        <div class="row">
-          {this.renderCampsite(this.props.campsite)}
-          {this.renderComments(this.props.campsite.comments)}
-        </div>
-      );
-    } else {
-      return (
-        <div class="row">
-          <div className="col-md-10 m-1 text-center font-weight-bold text-success">
-            <p className="p-2 text-center">
-              Select A Campsite To View Its Information
-            </p>
+        <div class="container">
+          <div class="row">
+            {this.renderCampsite(this.props.campsite)}
+            {this.renderComments(this.props.campsite.comments)}
           </div>
         </div>
       );
     }
+    return <div />;
   }
 }
