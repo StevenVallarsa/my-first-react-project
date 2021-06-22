ba21ed8590cf0b180a28dda881f5d220abca0acb
StevenVallarsa <orionsjv@gmail.com>
2021-06-22 18:13:54 -0400

    React Router setup

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Jumbotron } from "reactstrap";
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav() {
+    this.setState((prev) => ({ isNavOpen: !prev.isNavOpen }));
+  }
+
   render() {
     return (
       <>
@@ -16,9 +37,46 @@ class Header extends Component {
           </div>
         </Jumbotron>
 
-        <Navbar dark sticky="top">
+        <Navbar dark sticky="top" expand="md">
           <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="/assets/images/logo.png"
+                height="30"
+                width="30"
+                alt="NuCamp Logo"
+              />
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNav} />
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <i className="fa fa-home fa-lg"></i> Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/directory">
+                    <i className="fa fa-list fa-lg"></i> Directory
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/aboutus">
+                    <span style={{ minWidth: 100 }}>
+                      &nbsp;&nbsp;
+                      <i className="fa fa-info fa-lg" />
+                      &nbsp;&nbsp;&nbsp;
+                    </span>
+                    About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <i className="fa fa-address-card fa-lg"></i> Contact Us
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
           </div>
         </Navbar>
       </>
