a2055955dbcd61ad0bb3dab79cc725b328c70e66
StevenVallarsa <orionsjv@gmail.com>
2021-06-22 21:08:31 -0400

    React Router Params

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -47,7 +47,7 @@ function CampsiteInfo(props) {
       <div class="container">
         <div class="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.campsite.comments} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     );
